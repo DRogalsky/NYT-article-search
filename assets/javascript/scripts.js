59f536cc7e75f081ec286cb2c336dc81fd20b62b
Iman Gure <imangu99@gmail.com>
2019-06-10 14:35:36 -0700

    fire alarm

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,15 +1,19 @@
-$("#subit").on('click', function(){
-    var term = $("#searTerm");
-    var yearStart =$("#search");
-    var yearEnd =$();
-    var records = $();
-    
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + term +"&api-key=iBnmKatoqNtxhgPjQanm7Mad73AorZ2R&facet_fields= source&facet=true&begin_date="+yearStart+"&end_date="+yearEnd+"page=0";
-    
+$("#search").on('click', function(event){
+    event.preventDefault();
+    var term = $("#searchTerm").val();
+    var yearStart =$("#searchYear").val();
+    var yearEnd =$("searchEnd").val();
+    var records = $("#seachNumber").val();
+    console.log(term);
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + term + "&facet_fields= source&facet=true&begin_date=" + yearStart + "&end_date=" + yearEnd + "page=0" +"&api - key=iBnmKatoqNtxhgPjQanm7Mad73AorZ2R";
+    // $('#searchForm').on('submit',function(event){
+    //     event.preventDefault();
+    // })
     $.ajax({
         url:url,
         method:"GET"}).then(function(respond){
             var result = respond.data;
+            console.log(respond);
         })
         
     });
